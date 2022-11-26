97f2ed51420c952034af3d5341068c8378fd98ed
opesnoir <goodsurroundsme@outlook.com>
2022-11-26 11:29:56 +0100

    formulier onderdeel af, zowel de Formulier.jsx als in de App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Formulier from "./Components/Formulier/Formulier";
 
 
 
@@ -13,59 +14,39 @@ import './App.css';
 // 7. conditioneel renderen
 // 8. stateful components.
 
+//1. state
+//2. formulier
+//3. componenten
+//4. logo en styling
 
 function App() {
 
-  return (
-    <>
-      <div className="formulier-persoonsgegevens-container">
-        <form action="">
-          <fieldset>
-            <label htmlFor="input-firstname" className="item">Voornaam
-              <input type="text" name="username" id="input-firstname">
-            </label>
-            <br>
-              <label htmlFor="input-lastname">Achternaam
-                <input type="text" name="username" id="input-lastname">
-              </label>
-              <br>
-                <label htmlFor="user-age-number">Leeftijd<input type="number" placeholder="0" name="user-age"
-                                                                id="user-age-number">
-                </label>
-                <br>
-                  <label htmlFor="postcode">Postcode</label>
-                  <input type="text" pattern="[0-9]{4} && [A-Z]{2}" id="postcode">
-                    <!-- <input type="number" id="postcode" class="postcode" pattern="[0-9]{4}" placeholder="4 cijfers">
-                     <input type="text" id="postcode" class="postcode" pattern="[A-Z]{2}" placeholder="2 letters">-->
-                    <br>
-                      <label htmlFor="deliverance-frequency">Bezorgfrequentie</label>
-                      <select name="deliverance-frequency" id="deliverance-frequency">
-                        <option value="weekly">Iedere week</option>
-                        <option value="bi-weekly">Om de week</option>
-                        <option value="monthly">Iedere maand</option>
-                      </select>
-                      <br>
-                        <label htmlFor="delivery-moment-dag" && label htmlFor="delivery-moment-avond"></label>
-                        <input type="radio" name="moment" id="delivery-moment-dag" checked> Overdag </input>
-                        <input type="radio" name="moment" id="delivery-moment-avond"> 's Avonds </input>
-                        <!--  <input type="radio" name="delivery-moment" value="day" checked>Overdag</input>
-                          <input type="radio" name="delivery-moment" value="night">'s Avonds</input>-->
-                        <br>
-                          <label htmlFor="remarks">Opmerking</label>
-                          <textarea name="remark-textbox-area" id="remarks" cols="30" rows="10"></textarea>
-                          <!-- <input type="text" name="remark-textbox" id="remarks">-->
-                          <br>
-                            <input type="checkbox" name="agreement-check-box" id="agreement-check-box">
-                              <label htmlFor="agreement-check-box">Ik ga akkoord met de voorwaarden</label>
+    return (
+        <>
+            <div className="containerForm">
+                <Formulier
+                    formulierTitle="Formulier"
+                    firstName="Voornaam"
+                    lastName="Achternaam"
+                    ageNumber="Leeftijd"
+                    ageNumberPlaceholder={0}
+                    postcode="Postcode"
+                    postcodePlaceholder="4 cijfers en 2 letters"
+                    deliveranceFrequency="Bezorg frequentie"
+                    weekly="Iedere week"
+                    biWeekly="Om de week"
+                    monthly="Iedere maand"
+                    deliveryMomentDay="Overdag"
+                    deliveryMomentNight="'s Avonds"
+                    remark="Opmerking"
+                    agreementCheckBox="Ik ga akkoord met de voorwaarden"
+                    sendButton="Verzend"
 
-                              <br>
-                                <button type="submit">Verzend</button>
-          </fieldset>
-        </form>
-      </div>
+                />
+            </div>
 
-</>
-  );
+        </>
+    );
 }
 
 export default App;
---
+++ src/Components/Formulier/Formulier.jsx
@@ -0,0 +1,80 @@
+//1. React importeren met: import React from 'react';
+//2. formulier maken: rsc > tab;
+//3. formulier template maken;
+//3. vragen waarom <label htmlFor="postcode">{postcode}</label> <input type="text" id="postcode" pattern="[0-9]{4} && [A-Z]{2}" placeholder={postcodePlaceholder}/> niet werkt.
+//      hij geeft als foutmelding: insert requested input type, dat doe ik 4 cijfers en 2 letters;
+//      de alternatieve optie werkte ook niet:   <input type="number" id="postcode" class="postcode" pattern="[0-9]{4}" placeholder="4 cijfers">
+//        <input type="text" id="postcode" class="postcode" pattern="[A-Z]{2}" placeholder="2 letters">
+//       <br>
+//5. vragen of de kinderen nesten wel klopt, het lukt namelijk niet.
+//6. vragen of het normaal is dat de code zo lang is.
+
+import React from 'react';
+
+
+function Formulier({
+                       formulierTitle,
+                       firstName,
+                       lastName,
+                       ageNumber,
+                       ageNumberPlaceholder,
+                       postcode,
+                       postcodePlaceholder,
+                       deliveranceFrequency,
+                       weekly,
+                       biWeekly,
+                       monthly,
+                       deliveryMomentDay,
+                       deliveryMomentNight,
+                       remark,
+                       agreementCheckBox,
+                       sendButton
+                   }) {
+
+    return (
+        <>
+            <div>
+                <form>
+                    <h3>{formulierTitle}</h3>
+                    <label htmlFor="firstName"> {firstName} </label>
+                    <input type="text" name="userFirstName" id="firstName"/>
+                    <br/>
+                    <label htmlFor="lastName"> {lastName} </label>
+                    <input type="text" name="userLastName" id="lastName"/>
+                    <br/>
+                    <label htmlFor="ageNumber">{ageNumber}</label>
+                    <input type="number" min={0} placeholder={ageNumberPlaceholder} name="userAge" id="ageNumber"/>
+                    <br/>
+                    <label htmlFor="postcode">{postcode}</label>
+                    <input type="text" id="postcode" pattern="[0-9]{4} && [A-Z]{2}" placeholder={postcodePlaceholder}/>
+                    <br/>
+                    <label htmlFor="deliveranceFrequency">{deliveranceFrequency}</label>
+                    <select name="optionDeliveranceFrequency" id="deliveranceFrequency">
+                        <option value="weekly">{weekly}</option>
+                        <option value="biWeekly">{biWeekly}</option>
+                        <option value="monthly">{monthly}</option>
+                    </select>
+                    <br/>
+                    <div className="deliveryMoment">
+                        <label htmlFor="deliveryMomentDay">{deliveryMomentDay}</label>
+                        <input type="radio" name="moment" id="deliveryMomentDay" checked/>
+                        <label htmlFor="deliveryMomentNight">{deliveryMomentNight}</label>
+                        <input type="radio" name="moment" id="deliveryMomentNight"/>
+                    </div>
+                    <br/>
+                    <label htmlFor="remarks">{remark}</label>
+                    <textarea name="remarkArea" id="remarks" cols="30" rows="10"></textarea>
+                    <br/>
+                    <input type="checkbox" name="agreementBox" id="agreementCheckBox"/>
+                    <label htmlFor="agreementCheckBox">{agreementCheckBox}</label>
+                    <br/>
+                    <button type="submit">{sendButton}</button>
+                </form>
+            </div>
+        </>
+    );
+}
+
+export default Formulier;
+
+
